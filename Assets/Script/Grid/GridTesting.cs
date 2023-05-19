5e2ccc4f5af0769a72818227e0e3bb61bc6995ae
Sishi <sishi.hu17@gmail.com>
2023-05-19 16:17:20 +0200

    Setup grid

---
+++ Assets/Script/Grid/GridTesting.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridTesting : MonoBehaviour {
+
+    [SerializeField] Transform gridDebugObjectPrefab;
+    private GridSystem gridSystem;
+
+    private void Start() {
+        gridSystem = new GridSystem(10, 10, 2f);
+        gridSystem.CreateDebugObjects(gridDebugObjectPrefab);
+
+        Debug.Log(new GridPosition(5,7));
+    }
+
+    private void Update() {
+        Debug.Log(gridSystem.GetGridPosition(MouseWorld.GetPosition()));
+    }
+}
