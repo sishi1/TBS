1b48e8c8ea3ace1eda6bb56568eedb60b488eb8e
Sishi <sishi.hu17@gmail.com>
2023-06-13 11:12:19 +0200

    Added basic turn system

---
+++ Assets/Script/UI/TurnSystemUI.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TurnSystemUI : MonoBehaviour
+{
+
+    [SerializeField] private TextMeshProUGUI turnNumberText;
+    [SerializeField] private Button endTurnBtn;
+
+    private void Start() {
+        endTurnBtn.onClick.AddListener(() => {
+            TurnSystem.Instance.NextTurn();
+        });
+
+        TurnSystem.Instance.OnTurnChanged += TurnSystem_OnTurnChanged;
+
+        UpdateTurnText();
+    }
+
+    private void TurnSystem_OnTurnChanged(object sender, System.EventArgs e) {
+        UpdateTurnText();
+    }
+
+    private void UpdateTurnText() {
+        turnNumberText.text = "TURN " + TurnSystem.Instance.GetTurnNumber();
+    }
+}
